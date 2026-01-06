fc27d181f1ae347bd3f06831463779857e739045
Alex Ninov <a.ninov@InnovaSYS.BG>
2026-01-06 18:40:05 +0200

    solved StudentInformation

---
+++ Fundamentals-Section/Basic Syntax, Conditional Statements and Loops/Lab/StudentInformation.cs
@@ -0,0 +1,24 @@
+﻿namespace Fundamentals_Section.Basic_Syntax__Conditional_Statements_and_Loops.Lab
+{
+    public class StudentInformation
+    {
+        private string _studentName;
+        private int _studentAge;
+        private double _studentAverageGrade;
+
+        public StudentInformation(string name, int age, double grade)
+        {
+            _studentName = name;
+            _studentAge = age;
+            _studentAverageGrade = grade;
+        }
+
+        private string FormattingString(string name, int age, double grade) => $"Name: {name}, Age: {age}, Grade: {grade}";
+
+        public void PrintTaskResult()
+        {
+            string outputMessage = this.FormattingString(_studentName, _studentAge, _studentAverageGrade);
+            Console.WriteLine(outputMessage);
+        }
+    }
+}
--- Fundamentals-Section/Program.cs
+++ Fundamentals-Section/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Fundamentals_Section.Basic_Syntax__Conditional_Statements_and_Loops.Lab;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        StudentInformation studentInformation = new StudentInformation("John", 15, 5.40);
+        studentInformation.PrintTaskResult();
+    }
+}
